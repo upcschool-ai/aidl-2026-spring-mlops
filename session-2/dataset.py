eab69ff6b65cf8db3dbdf33831baab6005f0b7de
pcaselles <pcaselles22@gmail.com>
2026-03-03 16:25:48 +0100

    add session-2

---
+++ session-2/dataset.py
@@ -0,0 +1,17 @@
+import torch
+
+import pandas as pd
+from PIL import Image
+
+class MyDataset(torch.utils.data.Dataset):
+
+    def __init__(self, images_path, labels_path):
+        pass
+        
+
+    def __len__(self):
+        pass
+
+
+    def __getitem__(self, idx):
+        pass
